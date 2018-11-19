f6790054896be5d3726c45dab32ac4166265d96f
Navin Sasikumar <navinsasikumar@gmail.com>
2018-11-18 19:19:45 -0500

    Use query string params on the server side

--- server/src/lib/mycounts.js
+++ server/src/lib/mycounts.js
@@ -2,49 +2,49 @@ const Observations = require('./Observations');
 
 let observations = new Observations();
 
-let opts = {
-  //month: '4%2C5',
-  place_id: 2983,
-  verifiable: true,
-  captive: false
-};
-
-let userOpts = {
-  place_id: 2983,
-  user_id: 'navin_sasikumar',
-  verifiable: true,
-  captive: false
-};
-
 /* jshint ignore:start */
-const getTotals = async function(page = 1) {
-  opts.page = page;
+const getTotals = async function(query, page = 1) {
+  let opts = {
+    place_id: query.place_id || 2983,
+    verifiable: true,
+    captive: false,
+    page: page
+  };
+  if (query.months) opts.month = query.months
+
   let response = await observations.speciesCounts(opts);
   let total = response.total_results;
   let perPage = response.per_page;
   let thisPage = response.page;
   if (total - (thisPage * perPage) > 0) {
-    return response.results.concat(await getTotals(thisPage + 1));
+    return response.results.concat(await getTotals(query, thisPage + 1));
   } else {
     return response.results;
   }
 };
 
-const getUserTotals = async function(page = 1) {
-  userOpts.page = page;
+const getUserTotals = async function(query, page = 1) {
+  let userOpts = {
+    place_id: query.place_id || 2983,
+    user_id: query.user_id || 'navin_sasikumar',
+    verifiable: true,
+    captive: false,
+    page: page
+  };
+
   let response = await observations.speciesCounts(userOpts);
   let total = response.total_results;
   let perPage = response.per_page;
   let thisPage = response.page;
   if (total - (thisPage * perPage) > 0) {
-    return response.results.concat(await getUserTotals(thisPage + 1));
+    return response.results.concat(await getUserTotals(query, thisPage + 1));
   } else {
     return response.results;
   }
 };
 
-const missingSpecies = async function() {
-  const [totals, userTotals] = await Promise.all([getTotals(), getUserTotals()]);
+const missingSpecies = async function(query) {
+  const [totals, userTotals] = await Promise.all([getTotals(query), getUserTotals(query)]);
   let userTaxa = userTotals.map(function(taxon) { return taxon.taxon.name });
   let minus = totals.filter(function(taxon) {
     return userTaxa.indexOf(taxon.taxon.name) < 0;
--- server/src/routes/inat.js
+++ server/src/routes/inat.js
@@ -2,10 +2,7 @@ const myCounts = require('../lib/mycounts');
 
 const speciesCounts = async function(req, res) {
   res.set('Content-Type', 'application/json');
-  const diff = await myCounts.missingSpecies();
-  let data = {
-    message: 'Is anybody there?'
-  };
+  const diff = await myCounts.missingSpecies(req.query);
   console.log('Got all data');
   res.send(diff);
 };
