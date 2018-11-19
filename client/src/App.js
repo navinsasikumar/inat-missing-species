3fd2d384aa12117913bb424fa223e517ee986cda
Navin Sasikumar <navinsasikumar@gmail.com>
2018-11-18 21:00:50 -0500

    Add react router

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from 'react';
 import InfiniteScroll from 'react-infinite-scroller';
 import ReactTooltip from 'react-tooltip'
+import { Switch, Route } from 'react-router-dom'
+//import queryString from 'query-string'
 import './compiled/App.css';
 
 class TaxonImageLarge extends Component {
@@ -78,60 +80,6 @@ class Taxon extends Component {
   }
 }
 
-class Display extends Component {
-  render() {
-    console.log(this.props);
-    const iconicTaxa = {
-      plants: 47126,
-      reptiles: 26036,
-      insects: 47158,
-      birds: 3,
-      mammals: 40151,
-      amphibians: 20978,
-      fungi: 47170,
-      fishes: 47178,
-      molluscs: 47115,
-      arachnids: 47119,
-      protozoa: 47686,
-      diatoms: 48222,
-      other: 0
-    };
-
-    let simpleData = this.props.results;
-    let resultCount = this.props.results.length;
-
-    if (this.props.filter.length > 0) {
-      let filterTaxa = this.props.filter.map((name) => { return iconicTaxa[name]; });
-      simpleData = simpleData.filter((taxon) => {
-        return filterTaxa.indexOf(taxon.taxon.iconic_taxon_id) >= 0;
-      });
-    }
-
-    simpleData = simpleData.map((taxon, index) => {
-      let taxonElem;
-      let key = taxon.taxon.name;
-      if (index < 0) { //Making this always false for now
-        taxonElem = <React.Fragment><div key={key} className='col'><TaxonLarge taxon={taxon}/></div><div class="w-100"></div></React.Fragment>
-      } else {
-        taxonElem = <div key={key} className='col-xs-2'><Taxon taxon={taxon}/></div>
-      }
-      return (
-        taxonElem
-      )
-    });
-
-    let dispCount = 25;
-    let key = simpleData.length + '|' + this.props.filter.join('');
-
-    return (
-      <React.Fragment>
-        <StatusBar results={simpleData} />
-        <ResultsDisplay results={simpleData} count={resultCount} dispCount={dispCount} key={key}/>
-      </React.Fragment>
-    );
-  }
-}
-
 class ResultsDisplay extends Component {
   constructor(props) {
     super(props);
@@ -156,7 +104,7 @@ class ResultsDisplay extends Component {
     const loader = <div key={this.props.results.length} className="loader">Loading ...</div>;
     //let resultsDisp = this.loadItems(1);
 
-    if (this.props.results.length > 0) {
+    if (this.props.count > 0) {
       return (
         <InfiniteScroll
                   pageStart={0}
@@ -248,6 +196,99 @@ class FilterBar extends Component {
   }
 }
 
+class Display extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      results: []
+    };
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ results: res }))
+      .catch(console.error);
+  }
+
+  callApi = async () => {
+    //const queries = queryString.parse(this.props.location.search)
+    //console.log(queries);
+    let url = '/api/observations/species' + this.props.location.search;
+    const resp = await fetch(url);
+
+    window._resp = resp;
+
+    let text = await resp.text();
+
+    let data = null;
+    try {
+      data = JSON.parse(text); // cannot call both .json and .text - await resp.json();
+    } catch (e) {
+      console.err(`Invalid json\n${e}`);
+    }
+
+    if (resp.status !== 200) {
+      throw Error(data ? data.message : 'No data');
+    }
+
+    console.log(data);
+    return data;
+  };
+
+  render() {
+    console.log(this.props);
+    const iconicTaxa = {
+      plants: 47126,
+      reptiles: 26036,
+      insects: 47158,
+      birds: 3,
+      mammals: 40151,
+      amphibians: 20978,
+      fungi: 47170,
+      fishes: 47178,
+      molluscs: 47115,
+      arachnids: 47119,
+      protozoa: 47686,
+      diatoms: 48222,
+      other: 0
+    };
+
+    let simpleData = this.state.results;
+    let resultCount = this.state.results.length;
+
+    if (this.props.filter.length > 0) {
+      let filterTaxa = this.props.filter.map((name) => { return iconicTaxa[name]; });
+      simpleData = simpleData.filter((taxon) => {
+        return filterTaxa.indexOf(taxon.taxon.iconic_taxon_id) >= 0;
+      });
+    }
+
+    simpleData = simpleData.map((taxon, index) => {
+      let taxonElem;
+      let key = taxon.taxon.name;
+      if (index < 0) { //Making this always false for now
+        taxonElem = <React.Fragment><div key={key} className='col'><TaxonLarge taxon={taxon}/></div><div class="w-100"></div></React.Fragment>
+      } else {
+        taxonElem = <div key={key} className='col-xs-2'><Taxon taxon={taxon}/></div>
+      }
+      return (
+        taxonElem
+      )
+    });
+
+    let dispCount = 25;
+    let key = simpleData.length + '|' + this.props.filter.join('');
+
+    return (
+      <React.Fragment>
+        <StatusBar results={simpleData} />
+        <ResultsDisplay results={simpleData} count={resultCount} dispCount={dispCount} key={key}/>
+      </React.Fragment>
+    );
+  }
+}
+
 class MainBody extends Component {
   constructor(props) {
     super(props);
@@ -276,56 +317,22 @@ class MainBody extends Component {
     return (
       <React.Fragment>
         <FilterBar onFilterBarChange={this.handleFilterChange} />
-        <Display results={this.props.results} filter={this.state.filters}/>
+        <Switch>
+          <Route exact path='/' render={(props) => <Display {...props} filter={this.state.filters} />}/>
+        </Switch>
       </React.Fragment>
     )
   }
 }
 
 class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      results: []
-    };
-  }
-
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ results: res }))
-      .catch(console.error);
-  }
-
-  callApi = async () => {
-    const resp = await fetch('/api/observations/species');
-
-    window._resp = resp;
-
-    let text = await resp.text();
-
-    let data = null;
-    try {
-      data = JSON.parse(text); // cannot call both .json and .text - await resp.json();
-    } catch (e) {
-      console.err(`Invalid json\n${e}`);
-    }
-
-    if (resp.status !== 200) {
-      throw Error(data ? data.message : 'No data');
-    }
-
-    console.log(data);
-    return data;
-  };
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Philly CNC</h1>
         </header>
-        <MainBody results={this.state.results} />
+        <MainBody />
       </div>
     );
   }
