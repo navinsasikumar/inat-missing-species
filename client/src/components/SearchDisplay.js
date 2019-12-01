7bf51ed6f05f3882e9b685849294cda8b254cddb
Navin Sasikumar <navinsasikumar@gmail.com>
2019-12-01 11:55:39 -0500

    Add pagination to search

--- client/src/components/SearchDisplay.js
+++ client/src/components/SearchDisplay.js
@@ -18,13 +18,18 @@ class SearchDisplay extends Component {
     this.callApi = this.callApi.bind(this);
     this.isEqualArrays = this.isEqualArrays.bind(this);
     this.splitQueryStr = this.splitQueryStr.bind(this);
+    this.setObservationResults = this.setObservationResults.bind(this);
     this.handleSpeciesChange = this.handleSpeciesChange.bind(this);
     this.handleSpeciesSelect = this.handleSpeciesSelect.bind(this);
+    this.handlePageClick = this.handlePageClick.bind(this);
 
     this.state = {
       queryStr: '',
       results: [],
       loading: false,
+      totalResults: 0,
+      perPage: 0,
+      page: 1,
       errors: '',
       selectedSpecies: [],
       speciesInputValue: '',
@@ -45,10 +50,10 @@ class SearchDisplay extends Component {
   splitQueryStr = async (queryStr) => {
     const query = queryString.parse(queryStr);
     // console.log(query);
-    let selectedSpecies;
+    const newState = {};
     if (query.taxon_ids) {
       const getTaxaRes = await this.callApi(`/api/taxa/${query.taxon_ids}`);
-      selectedSpecies = getTaxaRes.map(taxon => (
+      newState.selectedSpecies = getTaxaRes.results.map(taxon => (
         {
           id: taxon.id,
           name: taxon.name,
@@ -56,7 +61,20 @@ class SearchDisplay extends Component {
         }
       ));
     }
-    this.setState({ selectedSpecies });
+    if (query.page) {
+      newState.page = query.page;
+    }
+    this.setState(newState);
+  }
+
+  setObservationResults = (res) => {
+    this.setState({
+      results: res.results,
+      loading: false,
+      totalResults: res.total_results,
+      perPage: res.per_page,
+      page: res.page,
+    });
   }
 
   callApi = async (url = '/api/observations') => {
@@ -73,7 +91,7 @@ class SearchDisplay extends Component {
     if (resp.status !== 200) {
       throw Error(data ? data.message : 'No data');
     }
-    return data.results;
+    return data;
   }
 
 
@@ -82,7 +100,7 @@ class SearchDisplay extends Component {
     this.setState({ speciesInputValue: searchStr });
     const url = `/api/taxa/autocomplete?search=${searchStr}`;
     this.callApi(url)
-      .then(res => this.setState({ speciesMatch: res }))
+      .then(res => this.setState({ speciesMatch: res.results }))
       .catch(e => this.setState({ errors: e }));
   }
 
@@ -94,19 +112,32 @@ class SearchDisplay extends Component {
     }));
   }
 
+  handlePageClick = (data) => {
+    let page = data.selected + 1;
+    if (page * this.state.perPage > 10000) {
+      page = Math.floor(10000 / this.state.perPage);
+      console.log('iNat API limitation');
+      console.log(page);
+    }
+    this.setState({ page });
+  }
+
   componentDidUpdate(prevProps, prevState) {
     const prevTaxonIds = prevState.selectedSpecies.map(taxon => taxon.id);
     const currTaxonIds = this.state.selectedSpecies.map(taxon => taxon.id);
-    if (!this.isEqualArrays(prevTaxonIds, currTaxonIds)) {
-      const queryStr = `taxon_ids=${currTaxonIds.join(',')}`;
-      const url = `/api/observations?${queryStr}`;
+    if (!this.isEqualArrays(prevTaxonIds, currTaxonIds) || prevState.page !== this.state.page) {
+      const queryObj = {};
+      if (currTaxonIds.length > 0) queryObj.taxon_ids = currTaxonIds.join(',');
+      if (this.state.page !== 1) queryObj.page = this.state.page;
+      const queryStr = queryString.stringify(queryObj);
+      const url = queryStr ? `/api/observations?${queryStr}` : '/api/observations';
 
       this.setState({ loading: true, queryStr }, () => {
         this.props.history.push(`/search?${this.state.queryStr}`);
       });
 
       this.callApi(url)
-        .then(res => this.setState({ results: res, loading: false }))
+        .then(res => this.setObservationResults(res))
         .catch(e => this.setState({ errors: e }))
         .finally(() => this.setState({ loading: false }));
     }
@@ -119,7 +150,7 @@ class SearchDisplay extends Component {
     } else {
       this.setState({ loading: true });
       this.callApi()
-        .then(res => this.setState({ results: res, loading: false }))
+        .then(res => this.setObservationResults(res))
         .catch(e => this.setState({ errors: e }))
         .finally(() => this.setState({ loading: false }));
     }
@@ -140,6 +171,10 @@ class SearchDisplay extends Component {
           results={this.state.results}
           loading={this.state.loading}
           errors={this.state.errors}
+          totalResults={this.state.totalResults}
+          perPage={this.state.perPage}
+          page={this.state.page}
+          handlePageClick={this.handlePageClick}
         />
       </SearchDisplayWrapper>
     );
--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -8,6 +8,7 @@ import {
   Container,
   Row,
 } from 'react-bootstrap';
+import ReactPaginate from 'react-paginate';
 import ObservationSquare from './ObservationSquare';
 import LoadingSpinner from './LoadingSpinner';
 
@@ -17,10 +18,35 @@ const SearchResultsWrapper = styled.div`
 
 const ColOverride = styled.div``;
 
+const PaginateRow = styled.div`
+  li {
+    margin: 10px 0;
+    padding: 5px 10px;
+    cursor: pointer;
+  }
+
+  li.disabled {
+    color: #333;
+    cursor: not-allowed;
+  }
+
+  li.break-me {
+    color: #333;
+  }
+
+  li.active {
+    background-color: rgb(41, 97, 145);
+  }
+`;
+
 class SearchResults extends Component {
   static propTypes= {
     results: PropTypes.array.isRequired,
     loading: PropTypes.bool.isRequired,
+    page: PropTypes.number,
+    perPage: PropTypes.number,
+    totalResults: PropTypes.number,
+    handlePageClick: PropTypes.func.isRequired,
   };
 
   render() {
@@ -37,11 +63,31 @@ class SearchResults extends Component {
       ));
     }
 
+    const pages = Math.ceil(this.props.totalResults / this.props.perPage);
+
     return (
       <SearchResultsWrapper>
         <Container>
           <Row className="grid justify-content-center">
             {rowContent}
+          </Row>
+          <Row className="grid justify-content-center">
+            <PaginateRow>
+              <ReactPaginate
+                previousLabel={'Previous'}
+                nextLabel={'Next'}
+                breakLabel={'...'}
+                breakClassName={'break-me'}
+                pageCount={pages}
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={5}
+                forcePage={this.props.page - 1}
+                onPageChange={this.props.handlePageClick}
+                containerClassName={'pagination'}
+                subContainerClassName={'pages pagination'}
+                activeClassName={'active'}
+              />
+            </PaginateRow>
           </Row>
         </Container>
       </SearchResultsWrapper>
